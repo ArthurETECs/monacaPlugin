d742be32721d6fb9874c9846271fb1862e31de93
ArthurETECs <thonyx.vieiradasilva@gmail.com>
2024-05-25 10:18:23 +0900

    ta ponto

--- www/components/plugins.js
+++ www/components/plugins.js
@@ -1,2 +1,21 @@
 // This is a JavaScript file
 
+$(document).on("click","#alerta",function(){
+
+    function retorno(){   
+    }
+    navigator.notification.alert("!!!ME AJUDAAAAAAAAA!!!",retorno,"AVISO!","ACEITO?");
+});
+$(document).on("click","#confirma",function(){
+    function confirma(buttonIndex){
+        if(buttonIndex  ==  "1"){
+            navigator.notification.alert("Escolheu a opçao A");
+        }else{
+            navigator.notification.alert("Escolheu a opçao B");
+        }
+    }
+    navigator.notification.confirm("Escolha A ou B",confirma,"Escolha:",['A','B']);
+});
+$(document).on("click","#beep",function(){
+    navigator.notification.beep(4);
+});
